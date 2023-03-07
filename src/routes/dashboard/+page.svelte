4f4027bea72cedf0f1c93e397f589061226e2f9f
Aayush Gakhar <aayush20006@iiitd.ac.in>
2023-03-07 11:03:32 +0530

    s-auth

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { page } from "$app/stores"
     const muscles = [
         'arms',
         'chest',
@@ -7,6 +8,23 @@
         'back'
     ];
 </script>
+
+
+{#if $page.data.session}
+<h1>Protected page</h1>
+<p>
+  This is a protected content. You can access this content because you are
+  signed in.
+</p>
+<p>Session expiry: {$page.data.session?.expires}</p>
+{:else}
+<h1>Access Denied</h1>
+<p>
+  <a href="/auth/signin">
+    You must be signed in to view this page
+  </a>
+</p>
+{/if}
 
 <h3>dashboard</h3>
 
