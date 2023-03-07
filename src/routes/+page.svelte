70895d28bffeae244cb78184648e44c3ccb79c2b
Aayush Gakhar <aayush20006@iiitd.ac.in>
2023-03-07 13:42:54 +0530

    s-auth name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,16 @@
+<script>
+    import { page } from "$app/stores"
 
+    const username = $page.data.session? $page.data.session.user?.name: 'Guest';
+    console.log($page.data.session);
+</script>
 
 <div>
+{#if $page.data.session}
+<h3>Hello {username }</h3>
 <a href="/dashboard">Dashboard</a>
+{:else}
+<a href="/auth/signin">Sign in</a>
+{/if}
 </div>
 
