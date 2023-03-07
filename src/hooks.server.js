b77117999a83330740418240b47673608175b604
Aayush Gakhar <aayush20006@iiitd.ac.in>
2023-03-07 15:57:47 +0530

    sb

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,28 +1,68 @@
-import {SvelteKitAuth} from '@auth/sveltekit';
-import GitHub from '@auth/core/providers/github';
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
-import { redirect } from '@sveltejs/kit';
-import { sequence } from '@sveltejs/kit/hooks';
+// import {SvelteKitAuth} from '@auth/sveltekit';
+// import GitHub from '@auth/core/providers/github';
+// import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+// import { redirect } from '@sveltejs/kit';
+// import { sequence } from '@sveltejs/kit/hooks';
 
-async function authorization({ event, resolve }) {
-	// Protect any routes under /authenticated
-	if (event.url.pathname.startsWith('/dashboard')) {
-		const session = await event.locals.getSession();
-		if (!session) {
-			throw redirect(303, '/auth/signin');
-		}
-	}
+// async function authorization({ event, resolve }) {
+// 	// Protect any routes under /authenticated
+// 	if (event.url.pathname.startsWith('/dashboard')) {
+// 		const session = await event.locals.getSession();
+// 		if (!session) {
+// 			throw redirect(303, '/auth/signin');
+// 		}
+// 	}
 
-	// If the request is still here, just proceed as normally
-	const result = await resolve(event, {
-		transformPageChunk: ({ html }) => html
-	});
-	return result;
-}
+// 	// If the request is still here, just proceed as normally
+// 	const result = await resolve(event, {
+// 		transformPageChunk: ({ html }) => html
+// 	});
+// 	return result;
+// }
 
-export const handle = sequence(
-  SvelteKitAuth({
-	  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })]
-  }),
-  authorization
-);
+// export const handle = sequence(
+//   SvelteKitAuth({
+// 	  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })]
+//   }),
+//   authorization
+// );
+// src/hooks.server.ts
+
+// import '$lib/supabase';
+import {
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY
+} from '$env/static/public';
+import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+// import type { Handle } from '@sveltejs/kit';
+
+export const handle = async ({ event, resolve }) => {
+  event.locals.supabase = createSupabaseServerClient({
+    supabaseUrl: PUBLIC_SUPABASE_URL,
+    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+    event
+  });
+
+  /**
+   * a little helper that is written for convenience so that instead
+   * of calling `const { data: { session } } = await supabase.auth.getSession()`
+   * you just call this `await getSession()`
+   */
+  event.locals.getSession = async () => {
+    const {
+      data: { session }
+    } = await event.locals.supabase.auth.getSession();
+    return session;
+  };
+
+  return resolve(event, {
+    /**
+     * There´s an issue with `filterSerializedResponseHeaders` not working when using `sequence`
+     *
+     * https://github.com/sveltejs/kit/issues/8061
+     */
+    filterSerializedResponseHeaders(name) {
+      return name === 'content-range';
+    }
+  });
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,17 @@
 <script>
     import { page } from "$app/stores"
+    import supabase from '$lib/supabase'
+    async function getData() {
+        const { data, error } = await supabase
+            .from('logs')
+            .select()
+        if (error) throw new Error(error.message)
+        
+        return data
+    }
 
     const username = $page.data.session? $page.data.session.user?.name: 'Guest';
-    console.log($page.data.session);
+
 </script>
 
 <div>
@@ -10,7 +19,20 @@
 <h3>Hello {username }</h3>
 <a href="/dashboard">Dashboard</a>
 {:else}
-<a href="/auth/signin">Sign in</a>
+<a href="/auth/login">Sign in</a>
+<a href="/auth/register">Sign up</a>
 {/if}
 </div>
+
+<h1>My favorite games</h1>
+{#await getData()}
+  <p>Fetching data...</p>
+{:then data}
+  {#each data as row}
+    <li>{row.name}</li>
+  {/each}
+{:catch error}
+  <p>Something went wrong while fetching the data:</p>
+  <pre>{error}</pre>
+{/await}
 
